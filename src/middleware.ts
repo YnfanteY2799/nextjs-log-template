5700ed813d5f32a1cb814f374de8bcf7127acb00
Yassett J Ynfante Jimenez <49245097+YnfanteY2799@users.noreply.github.com>
2023-03-19 14:37:11 -0400

    +added tailwind

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,13 @@
 import { NextResponse, NextRequest } from "next/server";
 import { jwtVerify } from "jose";
+import { RequestCookies } from "next/dist/compiled/@edge-runtime/cookies";
 
 export async function middleware({ nextUrl, cookies, url }: NextRequest) {
   const cok = cookies.get("tok");
 
   if (!cok) return NextResponse.redirect(new URL("/login", url));
 
+  console.log("Cookie is : ", cok);
   try {
     const { payload } = await jwtVerify(cok, "secret");
   } catch (e) {}
